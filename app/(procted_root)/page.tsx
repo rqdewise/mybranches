913b660ff7cd0f91785b907506aa28d72bfd3c78
rqdewise <rqdeleon77@yahoo.com>
2023-09-01 18:12:45 +0800

    change bloqoute to p

--- app/(procted_root)/page.tsx
+++ app/(procted_root)/page.tsx
@@ -2,7 +2,7 @@
 import React from "react"
 import Image from "next/image"
 
-import { ModalProvider } from '@/providers/modal-provider'
+import { ModalProvider } from "@/providers/modal-provider"
 import { Banner } from "@/components/banner"
 
 
@@ -18,13 +18,13 @@ export default function Home() {
             <Image 
                 width={150}
                 height={150}
-                src={'/dp.jpg'}
+                src={"/dp.jpg"}
                 alt="Ralf" 
                 className="rounded-full"
             />
-            <blockquote className="w-[150px] inline-block max-w-[750px] text-sm text-muted-foreground sm:text-md text-center "> 
+            <p className="w-[150px] inline-block max-w-[750px] text-sm text-muted-foreground sm:text-md text-center"> 
                 I appreciate you coming here. I am eager to collaborate with you for this years ministry. Let us do the work as a chidren of light!
-            </blockquote>
+            </p>
 
         </section>
     </main>
